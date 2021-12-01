deb1fdfbad6272a4869a0137e7da286cd254556d
mohamad-gilang <mohamad_gilang@student.ub.ac.id>
2021-12-01 21:04:45 +0700

    booking.in app

--- app/src/main/java/com/example/bookingin/Register.java
+++ app/src/main/java/com/example/bookingin/Register.java
@@ -4,28 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
+public class Register extends AppCompatActivity implements View.OnClickListener {
 
-    Button _btnLogin;
-    TextView _toRegister;
+    Button _btnRegister;
+    TextView _toLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        _toRegister = findViewById(R.id.toLogin);
+        setContentView(R.layout.activity_register);
+        _toLogin = findViewById(R.id.toLogin);
 
-        _toRegister.setOnClickListener(this);
+        _toLogin.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(v == _toRegister){
-            Intent intent = new Intent(Login.this, Register.class);
+        if (v == _toLogin ){
+            Intent intent = new Intent(Register.this, Login.class);
             startActivity(intent);
         }
     }
